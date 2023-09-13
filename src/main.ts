3f16eb117a40a34b644f908859aed70c31eebc02
BrunoDeye <bruno.rodrigues@deye.solar>
2023-09-13 16:53:27 -0300

    fix: urls

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'https://calculadora-eosin-chi.vercel.app',
-      'https://main--imaginative-praline-c53240.netlify.app/',
-      'https://template-sozonome-biswimwh3-brunodeye.vercel.app/',
+      'https://main--imaginative-praline-c53240.netlify.app',
+      'https://template-sozonome-biswimwh3-brunodeye.vercel.app',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
