9b090223b2d47ed0d9a85269f5235fb0c320b73a
BrunoDeye <bruno.rodrigues@deye.solar>
2023-09-13 16:46:12 -0300

    feat: add urls to enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://calculadora-eosin-chi.vercel.app'],
+    origin: [
+      'https://calculadora-eosin-chi.vercel.app',
+      'https://main--imaginative-praline-c53240.netlify.app/',
+      'https://template-sozonome-biswimwh3-brunodeye.vercel.app/',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
